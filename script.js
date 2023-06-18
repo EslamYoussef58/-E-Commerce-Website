c36c6c1b68c3cfaa3096bd037278083dc3f5f4a1
EslamYoussef58 <112953593+EslamYoussef58@users.noreply.github.com>
2023-06-19 02:27:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+
+const navElems = [navOpenBtn, navCloseBtn];
+
+for(let i =0; i < navElems.length; i ++){
+    navElems[i].addEventListener("click", function (){
+        navbar.classList.toggle("active")
+    })
+} 
+
+
+
+const searchContainer = document.querySelector("[data-search-wrapper]");
+const searchBtn = document.querySelector("[data-search-btn]");
+
+searchBtn.addEventListener("click", function() {
+
+    searchContainer.classList.toggle("active");
+})
+
+
+
+
+
+
+
+
+const panelBtns = document.querySelectorAll("[data-panel-btn]");
+const sidePanels = document.querySelectorAll("[data-side-panel]");
+
+for(let i = 0; i < panelBtns.length; i++){
+    panelBtns[i].addEventListener("click", function (){
+        let clickedElemDataValue = this.dataset.panelBtn;
+
+        for( let i = 0; i <sidePanels.length; i++){
+
+            if (clickedElemDataValue === sidePanels[i].dataset.sidePanel){
+                sidePanels[i].classList.toggle("active")
+            }else{
+                sidePanels[i].classList.remove("active")
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const backToBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function(){
+    window.scrollY >= 100 ? backToBtn.classList.add("active")
+    : backToBtn.classList.remove("active")
+})
+
+
+
